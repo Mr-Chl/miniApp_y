b4ead32339125605954261a6067a59c31b35d49a
Mr-Chl <chenhelong6@sina.com>
2021-03-09 17:11:43 +0800

    add

--- config/config.js
+++ config/config.js
@@ -1,4 +1,4 @@
-const BASE_URL = "https://api.chenhelong.cn"; // 线上环境
+const BASE_URL = "https://api.chenhelong.top"; // 线上环境
 // const BASE_URL = "http://api.chenhelong.cn:3000";  // 测试环境
 
 const IMAGE_CDN_URL07 = "https://img07.miyabaobei.com"
